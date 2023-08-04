5a52344966192a9e38ab0ce9d5a077337c87f1f4
greenfest <1greenfest@gmail.com>
2023-08-04 16:01:29 +0500

    edited .header__menu and style.scss

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,4 @@
 $(function () {
-  const headerBurger = document.querySelector(".header__burger");
-  const headerMenu = document.querySelector(".header__menu");
-
-  headerBurger.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (headerMenu.style.visibility === "hidden") {
-      headerMenu.style.visibility = "visible";
-      headerMenu.style.opacity = "1";
-    } else {
-      headerMenu.style.visibility = "hidden";
-      headerMenu.style.opacity = "0";
-    }
-  });
-
   $(".product__slider").slick({
     infinite: false,
     speed: 500,
